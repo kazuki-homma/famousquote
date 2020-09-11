91b72f9c3633df64abb75345a913761465f88c88
本間千城 <>
2020-09-12 03:17:55 +0900

    display famous quotes table & create add.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+const db = require('../models');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.FamousQuotes.findAll().then(contents => {
+    var data = {
+      title: 'Famous Quote',
+      content: contents
+    }
+    res.render('index', data);
+  });
 });
 
 module.exports = router;
