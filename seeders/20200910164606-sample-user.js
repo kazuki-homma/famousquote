9f784c1a4b8b18466258e259698fdafad5f230d5
本間千城 <>
2020-09-12 01:56:56 +0900

    create seed

--- seeders/20200910164606-sample-user.js
+++ seeders/20200910164606-sample-user.js
@@ -2,23 +2,46 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('Users', [
+      {
+        name: 'Kazuki',
+        pass: '15921592',
+        mail: 'mutant.guppy@gmail.com',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Taro',
+        pass: 'yamada',
+        mail: 'taro@yamada.com',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Hanako',
+        pass: 'flower',
+        mail: 'hanako@flower.com',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Jiro',
+        pass: 'change',
+        mail: 'jiro@gchange.com',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Sachiko',
+        pass: 'happy',
+        mail: 'sachiko@happy.com',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('User', null, {});
   }
 };
