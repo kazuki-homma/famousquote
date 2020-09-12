cf1a2132b7b2bdd374892ba8d08617dd83922a03
本間千城 <>
2020-09-12 09:31:37 +0900

    crud update user

--- routes/users.js
+++ routes/users.js
@@ -32,5 +32,25 @@ router.post('/add',(req,res,next) => {
   });
 });
 
+router.get('/edit',(req,res,next)=>{
+  db.User.findByPk(req.query.id)
+  .then(usr => {
+    var data = {
+      title: 'Users/Edit',
+      form: usr
+    }
+    res.render('users/edit', data);
+  });
+});
+
+router.post('/edit',(req,res,next)=> {
+  db.User.findByPk(req.body.id)
+  .then(usr => {
+    usr.name = req.body.name;
+    usr.pass = req.body.pass;
+    usr.mail = req.body.mail;
+    usr.save().then(()=>res.redirect('/users'));
+  });
+});
 
 module.exports = router;
